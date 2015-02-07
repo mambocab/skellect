3ab16c7e68eac673b243ce78b31d418001eb6bd1
Jim Witschey <jim.witschey@gmail.com>
2015-02-07 17:56:40 -0500

    switches from prop -> it... property notation

--- test-suite/SkellectSpec.hs
+++ test-suite/SkellectSpec.hs
@@ -2,7 +2,7 @@ module SkellectSpec (spec) where
 
 import Skellect (skellect)
 import Test.Hspec (describe, it, shouldBe, Spec)
-import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck (property)
 
 spec :: Spec
 spec =
@@ -10,6 +10,6 @@ spec =
         it "returns unit" $
 	    skellect `shouldBe` ()
 
-	prop "is equal to unit" $
-	    \x -> skellect == x
+	it "is equal to unit" $ property $
+	    \x -> skellect `shouldBe` x
 
