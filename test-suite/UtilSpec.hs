d89e86b57928e69b1024d203e2d39c1f65745d44
Jim Witschey <jim.witschey@gmail.com>
2015-02-08 01:23:38 -0500

    adds QuickCheck properties to tests

--- test-suite/UtilSpec.hs
+++ test-suite/UtilSpec.hs
@@ -2,6 +2,19 @@ module UtilSpec (spec) where
 
 import Skellect.Utils (nonEmptyLines)
 import Test.Hspec (describe, it, shouldBe, Spec)
+import Test.QuickCheck (property)
+
+validLineSplits :: String -> [String] -> Bool
+validLineSplits "" [] = True
+validLineSplits _  [] = False
+validLineSplits "" _  = False
+validLineSplits ('\n':xs) ys = validLineSplits xs ys
+validLineSplits (x:xs) (y:ys)
+    | x == head y = validLineSplits xs (remainingSplit (tail y: ys))
+    | otherwise   = False
+        where
+            remainingSplit ("":as) = as
+            remainingSplit as = as
 
 spec :: Spec
 spec =
@@ -14,4 +27,8 @@ spec =
             nonEmptyLines "just\nfine\n" `shouldBe` ["just","fine"]
         it "ignores the empty string between two newlines" $
             nonEmptyLines "what\n\nnow?" `shouldBe` ["what","now?"]
+        it "doesn't let empty strings in its output" $ property $
+            \s -> "" `notElem` nonEmptyLines s
+        it "always provides a valid split" $ property $
+            \s -> validLineSplits s (nonEmptyLines s)
 
