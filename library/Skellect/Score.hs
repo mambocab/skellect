0e05bc0a9bed6c27e7f82ce3b86370f6576878d0
Jim Witschey <jim.witschey@gmail.com>
2015-02-16 00:49:58 -0500

    fixes order of cases

--- library/Skellect/Score.hs
+++ library/Skellect/Score.hs
@@ -4,9 +4,9 @@ import Data.List (tails)
 import Control.Applicative ((<$>))
 
 matchLength :: Eq a => [a] -> [a] -> Maybe Integer
+matchLength [] _ = Just 0
 matchLength query@(q:qs) (c:cs) = (+1) <$> matchLength rq cs
     where rq = if q == c then qs else query
-matchLength [] _ = Just 0
 matchLength _ [] = Nothing
 
 suffixesStartingWith :: Eq a => a -> [a] -> [[a]]
