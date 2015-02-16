b78a274814a81f9227a8ee66e60dd5cb1c5984b2
Jim Witschey <jim.witschey@gmail.com>
2015-02-16 00:20:04 -0500

    improves testing rig

--- t.sh
+++ t.sh
@@ -1,4 +1,12 @@
+# fail fast if any command dies with an error
+set -e
+# debug output: print each command before printing
 set -x
 
-cabal build && cabal exec -- runhaskell test-suite/HLint.hs && cabal exec -- runhaskell -ilibrary -itest-suite test-suite/Spec.hs
+# build, silencing normal output but allowing errors
+cabal build >/dev/null
+# lint
+cabal exec -- runhaskell test-suite/HLint.hs
+# test
+cabal exec -- runhaskell -ilibrary -itest-suite test-suite/Spec.hs
 
